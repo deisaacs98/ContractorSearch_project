318896362561b6d3bfac82bdcda072c00cc147e7
deisaacs98 <deisaacs98@gmail.com>
2021-03-12 07:31:56 -0600

    Registered Google Maps service.

--- ContractorSearch/Startup.cs
+++ ContractorSearch/Startup.cs
@@ -49,6 +49,7 @@ namespace ContractorSearch
             services.AddRazorPages();
             services.AddSignalR();
             services.AddTransient<TwilioService>();
+            services.AddTransient<GoogleMapsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
